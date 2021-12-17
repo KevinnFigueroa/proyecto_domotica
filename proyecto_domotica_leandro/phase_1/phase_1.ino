9b6af4db3132258f649db52a8e9b8a34facbd8a5
KevinnFigueroa <57240542+KevinnFigueroa@users.noreply.github.com>
2021-12-16 23:32:58 -0300

    Add files via upload

---
+++ proyecto_domotica_leandro/phase_1/phase_1.ino
@@ -0,0 +1,28 @@
+#define tecla1 19
+#define tecla2 18
+#define rele 23
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(tecla1, INPUT);
+  pinMode(tecla2, INPUT);
+  pinMode(rele, OUTPUT);
+}
+
+void loop() {
+  int teclaValue = digitalRead(tecla);   
+  //TODO: Hacer la combinacion con las 2 teclas para una luz
+  //TODO: Si esta encendido el foco, al apretar cualquiera de las dos teclas, se apaga y viceversa
+  
+  if( teclaValue == 1){
+    digitalWrite(rele, HIGH);
+    digitalWrite(2, HIGH);
+  }else{
+   digitalWrite(rele, LOW);
+   digitalWrite(2, LOW); 
+  }
+  
+  Serial.println("Imprimimos valor de la tecla");  
+  Serial.println(teclaValue);
+  //delay(500);
+}
