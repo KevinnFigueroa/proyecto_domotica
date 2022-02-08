d4a101f7f964aa1f659ede2115360298353d10bb
kevinfigg <kevinfigueroadavid2000@gmail.com>
2022-02-08 20:54:52 -0300

    feat: agregando control de teclas manual en el loop y variables para manejar estados anteriores

--- phase_2/phase_2.ino
+++ phase_2/phase_2.ino
@@ -1,18 +1,35 @@
-
 #include <WiFi.h>
 #include <WiFi.h>
 #include <WebSocketsServer.h>
 #include <string.h>
 #include <ArduinoJson.h>
-#include <PubSubClient.h>
+#include <PubSubClient.h> 
+
+// RELES
+#define releSimple 23
+#define releCombinadaDoble 24
+#define releCombinadaTriple 25
+
+// A DEFINIR
+
+// TECLA SIMPLE
+#define teclaSimple 13
+
+// TECLA COMBINADA DOBLE
+#define teclaCombinadaDoble1 14
+#define teclaCombinadaDoble2 15
+
+// TECLA COMBINADA TRIPLE
+#define teclaCombinadaTriple1 16
+#define teclaCombinadaTriple2 17
+#define teclaCombinadaTriple3 18
 
 
-#define rele 23
+#define MQTTpubQos 2
 
 WiFiClient espClient;
-WebSocketsServer webSocket = WebSocketsServer(81);
-
 PubSubClient client(espClient);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
 
 // Esto debe guardarse en la memoria EEPROM para restaurar internet en caso de corte de luz
@@ -22,7 +39,7 @@ const char* password = "";
 // MQTT Broker
 const char *mqtt_broker = "192.168.0.16";
 const char *topic = "";
-const char *mqtt_username = "esp32";
+const char *mqtt_username = "esp32/test";
 const char *mqtt_password = "public";
 const char *will_topic = "";
 const int mqtt_port = 1883;
@@ -33,15 +50,15 @@ const char* APpassword = "12345678";
 
 // IDENTIDAD DEL DISPOSITIVO, Esto se va a devolver cada vez que la app se sincronice por WS llamando al evento /device_information
 // TODO:esto debe guardarse en la memoria EEPROMM 
-//const char* roomName = "";
-//const char* deviceId = "";
+const char* user = "";
+const char* room = "";
 
 const char* topicMqttDevice = "";
 
 // Esto vendrá hardcodeado dependiendo del dispositivo
 const char* typeOfDevice = "light";
 
-#define MQTTpubQos          2
+ char* configurationDevice = "configuration_device/";
 
 void decodeMessage(uint8_t num, char* json){
   // Leer Json
@@ -67,15 +84,13 @@ void decodeMessage(uint8_t num, char* json){
   }
 
   if(eventName == "/set_information"){
-      // Debo generar conexión a servidor MQTT y suscribirme a un tópico
-
-      //roomName = doc["message"]["room_name"];
-      //deviceId = ssid = doc["message"]["device_id"];
-      
+          
       ssid = doc["message"]["ssid"];
       password = doc["message"]["password"];
       
       topic = doc["message"]["topic_mqtt_device"];
+      user = doc["message"]["user"];
+      room = doc["message"]["room"];
       
       // Este es el ultimo evento de la configuracion de dispositivo desde la APP, para este entonces, ya tendré cargado los datos del ssid y password en la placa y solo debo conectarme
       
@@ -83,45 +98,17 @@ void decodeMessage(uint8_t num, char* json){
        setupWifi(ssid, password);
 
       // TODO: UNA VEZ QUE ME CONECTÉ A INTERNET, NECESITO SUSCRIBIRME AL TOPICO QUE ME DA LA APP Y GUARDAR ESE VALOR EN LA MEMORIA EEPROM IGUAL QUE LA PASSWORD Y SSID DEL WIFI
-
-      // TODO: ESTE FEEDBACK NO LO ESTOY DANDO YA QUE AL CONECTARME A INTERNET, ME FUI DE LA CONEXION SOCKET Y LA APP NUNCA RECIBE EL EVENTO
-      // TODO: Necesito darle Feedback a la app de que se recibió este evento
-      
       response["event_name"] = "/set_information_response";
-      //response["type_of_device"] = typeOfDevice;
-
-      //TODO: enviar por WS este response
       
     }
-
-  
-    if(eventName == "/set_credentials_wifi"){
-      // TODO: Debo generar conexión a servidor MQTT y suscribirme a un tópico
-
-      //ssid = doc["message"]["ssid"];
-      //password = doc["message"]["password"];
-
-      // Una vez recibimos la ssid y password desde App, intentamos conectarnos a internet
-      //setupWifi(ssid, password);
-
-      // TODO: Necesito darle Feedback a la app de que se recibió este evento
-      response["event_name"] = "/set_credentials_wifi_response";
-      response["type_of_device"] = typeOfDevice;
-
-      //TODO: enviar por WS este response
-    }
-    
-
-    // TODO: Este evento lo pide la app para tener los datos del dispositivo en ella
+ 
     if(eventName == "/device_information"){
       
       // TODO: Necesito darle Feedback a la app de que se recibió este evento
       response["event_name"] = "/device_information_response";
-      response["on"] = true;
-      //response["room_name"] = roomName;
       response["type_of_device"] = typeOfDevice;
-      //TODO: enviar por WS este response
 
+      //TODO: enviar por WS este response
     }
 
     serializeJson(response, responseBuffer);
@@ -131,16 +118,53 @@ void decodeMessage(uint8_t num, char* json){
 
 
  void callback(char *topic, byte *payload, unsigned int length) {
- Serial.print("Message arrived in topic: ");
- Serial.println(topic);
- Serial.print("Message:");
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  
+  String topicStr = topic; 
+  String stringJson = String(( char *) payload);
+ 
+  // Leer Json
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, stringJson);
+
+  // Responder Json
+  DynamicJsonDocument response(1024);
+  char responseBuffer[100];
+
  for (int i = 0; i < length; i++) {
      Serial.print((char) payload[i]);
  }
+
+ String eventName = doc["event_name"];  
+
+ if (eventName == "/change_state"){
+  int state = doc["message"]["state"];
+
+  if(state == 1){
+    digitalWrite(2, HIGH);          
+   } else if(state == 0 ){    
+    digitalWrite(2, LOW);
+   }
+
+    response["event_name"] = "/change_state_response";
+    response["topic_mqtt_device"] = topicStr;
+    response["state"] = digitalRead(2); 
+ 
+ }
+
+ if (eventName == "/finish_configuration"){
+  // Este evento no nos interesa
+  return;
+ }
+
+ serializeJson(response, responseBuffer);
+ client.publish(topic, responseBuffer, MQTTpubQos);
+
  Serial.println();
  Serial.println("-----------------------");
-}
-  
+}  
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
     switch(type) {
@@ -198,9 +222,10 @@ void setupWifi(const char* ssid,const char* password){
 
       // Una vez que sabemos que hay internet, intentamos conectarnos
       connectToMqtt();
+      
     }else {
       
-    setupAccessPoint();
+      setupAccessPoint();
       
     }
 
@@ -223,6 +248,7 @@ void connectToMqtt(){
  //connecting to a mqtt broker
  client.setServer(mqtt_broker, mqtt_port);
  client.setCallback(callback);
+ 
  int retryConnections = 0;
  
  while (!client.connected() && retryConnections != 4) {
@@ -242,22 +268,44 @@ void connectToMqtt(){
  if(retryConnections != 4) {
    Serial.print("Suscribiendome y publicando en topico ");
    Serial.print(topic);
- 
- // publish and subscribe
- client.publish(topic, "Hola desde ESP32 !!", MQTTpubQos);
- client.subscribe(topic);
+
+     // Responder Json
+    DynamicJsonDocument response(1024);
+    char responseBuffer[100];
+  
+    response["user"] = user;
+    response["device"]["room"] = room;
+    response["device"]["type_of_device"] = typeOfDevice;
+    response["device"]["topic_mqtt_device"] = topic;
+
+   serializeJson(response, responseBuffer); 
+
+   // TODO: será que no puedo enviar el mensaje desde el esp porque es muy largo el topico??
+
+   // Este evento lo va a recibir el backend y va a generar una bandera de este dispositivo para luego avisar a la app
+   client.publish(configurationDevice, responseBuffer, MQTTpubQos);
+   client.subscribe(topic);
  }
   
 }
 
 void setup()
 {
+
     Serial.begin(115200);
     delay(10);
 
+    pinMode(teclaSimple, INPUT);
+    pinMode(teclaCombinadaDoble1,INPUT);
+    pinMode(teclaCombinadaDoble2,INPUT);
+    pinMode(teclaCombinadaTriple1, INPUT);
+    pinMode(teclaCombinadaTriple2,INPUT);
+    pinMode(teclaCombinadaTriple3,INPUT);
+
     // Los pines a los que se va a apuntar deben guardarse en la memoria eeprom 
-    pinMode(rele, OUTPUT);
-    pinMode(2,OUTPUT);
+    pinMode(releSimple, OUTPUT);
+    pinMode(releCombinadaDoble,OUTPUT);
+    pinMode(releCombinadaTriple,OUTPUT);
 
     setupWifi(ssid, password);
 
@@ -265,13 +313,75 @@ void setup()
     // connectToMqtt();
     
     // start webSocket server
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+}
+
+
+// ESTADOS ANTERIORES DE LAS TECLAS COMBINADAS DOBLES
+int beforeStateTeclaDobleValue1 = 0;
+int beforeStateTeclaDobleValue2 = 0;
+
+// ESTADOS ANTERIORES DE LAS TECLAS COMBINADAS TRIPLES
+int beforeStateTeclaTripleValue1 = 0;
+int beforeStateTeclaTripleValue2 = 0;
+int beforeStateTeclaTripleValue3 = 0;
+
+void readLightKeys(){
+  // Agregar MQTT A TODOS ESTOS CAMBIOS DE ESTADO, ESCRIBIENDO A LOS TOPICOS
+  
+  // Tecla simple
+  
+  int teclaSimpleValue = digitalRead(teclaSimple);
+  digitalWrite(releSimple, teclaSimpleValue);          
+   
+  // Tecla combinada doble
+
+  int teclaDoble1Value = digitalRead(teclaCombinadaDoble1);
+  int teclaDoble2Value = digitalRead(teclaCombinadaDoble2);
+
+  if(teclaDoble1Value != beforeStateTeclaDobleValue1 || teclaDoble2Value != beforeStateTeclaDobleValue2){
+    // Leo en qué estado está la luz
+    int val = digitalRead(releCombinadaDoble);
+    
+    // Cambio al estado contrario al que estaba
+    if(val == 0){
+      digitalWrite(releCombinadaDoble, 0);  
+    }else{
+      digitalWrite(releCombinadaDoble, 1);
+     }          
+    
+   }
+
+
+  // Tecla combinada triple
+  
+  int teclaTriple1Value = digitalRead(teclaCombinadaTriple1);
+  int teclaTriple2Value = digitalRead(teclaCombinadaTriple2);
+  int teclaTriple3Value = digitalRead(teclaCombinadaTriple3);
+
+
+  if(teclaTriple1Value != beforeStateTeclaTripleValue1 || teclaTriple2Value != beforeStateTeclaTripleValue2 || teclaTriple3Value != beforeStateTeclaTripleValue3){
+    // Leo en qué estado está la luz
+    int val = digitalRead(releCombinadaTriple);
+    
+    // Cambio al estado contrario al que estaba
+    if(val == 0){
+      digitalWrite(releCombinadaTriple, 0);  
+    }else{
+      digitalWrite(releCombinadaTriple, 1);
+     }          
+    
+   }
+  
 }
 
 
 void loop()
 {
+   // Control Manual de luces
+   readLightKeys();
+   // TODO: aqui deberia ir preguntando en un futuro el estado de la conexion a internet, si está conectado entonces prendemos led VERDE y sino EN ROJO.
    client.loop();
    webSocket.loop();
 }
